ba02f494f7cf6e766ddf18eadd800c82a08b95c4
DzeBaKuEso <dzeblebaku@gmail.com>
2025-05-27 14:00:01 -1200

    api/v1/auth/basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
-""" BasicAuth module
-"""
+"""Basic Auth module"""
 from api.v1.auth.auth import Auth
 
 
 class BasicAuth(Auth):
-    """BasicAuth class that inherits from Auth.
-    For now, it's an empty implementation.
-    """
-    pass
+    """BasicAuth inherits from Auth"""
+    
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """
+        Returns the Base64 part of the Authorization header for Basic Authentication.
+        """
+        if authorization_header is None:
+            return None
+        if not isinstance(authorization_header, str):
+            return None
+        if not authorization_header.startswith("Basic "):
+            return None
+        return authorization_header.split(' ', 1)[1]
