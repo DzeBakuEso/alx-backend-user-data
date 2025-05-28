aab78ab853905733a23fc159b709c86b9725cf41
DzeBaKuEso <dzeblebaku@gmail.com>
2025-05-27 14:03:06 -1200

    api/v1/auth/basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """Basic Auth module"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
     """BasicAuth inherits from Auth"""
-    
+
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
         """
         Returns the Base64 part of the Authorization header for Basic Authentication.
@@ -17,3 +18,18 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header.split(' ', 1)[1]
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """
+        Returns the decoded value of a Base64 string.
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            base64_bytes = base64_authorization_header.encode('utf-8')
+            decoded_bytes = base64.b64decode(base64_bytes)
+            return decoded_bytes.decode('utf-8')
+        except Exception:
+            return None
